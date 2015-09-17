3060a14e4a128bc939c0dac4be625fd5814087c8
David Chen <chenwei2000121@163.com>
2015-09-17 18:03:00 +0800

    add Gib

---
+++ CW Study Tool 3 for PC/Gib.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_Study_Tool_3_for_PC
+{
+    class Gib
+    {
+        public static string hostpath = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        public static string hostpaths = hostpath + "\\CW Study Tool 3\\";
+    }
+}
