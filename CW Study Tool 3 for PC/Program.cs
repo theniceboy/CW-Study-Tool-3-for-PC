a5281c2fb7dc98c62605ba470eb1300e414997cb
David Chen <chenwei2000121@163.com>
2015-09-17 16:44:32 +0800

    initial commit

---
+++ CW Study Tool 3 for PC/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CW_Study_Tool_3_for_PC
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
